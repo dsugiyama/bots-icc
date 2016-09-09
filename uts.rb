cd1f6c5e3f1b7f48f74e4fafe661b432a22e651f
Daisuke Sugiyama <dsugiyama@outlook.jp>
2016-09-09 15:05:21 +0900

    fix uts.rb

--- uts.rb
+++ uts.rb
@@ -7,27 +7,27 @@ puts 'unit: nodes/sec'
 puts "# of threads: #{num_threads.join(' ')}"
 puts
 
-puts 'serial'
-run 'bin/uts.icc.serial -f inputs/uts/small.input'
-puts
-
-puts 'untied'
-num_threads.each do |n|
-  run "OMP_NUM_THREADS=#{n} bin/uts.icc.omp-tasks -f inputs/uts/small.input"
-end
-puts
-
-puts 'tied'
-num_threads.each do |n|
-  run "OMP_NUM_THREADS=#{n} bin/uts.icc.omp-tasks-tied -f inputs/uts/small.input"
-end
-
-def run(command)
-  iter_cmd = "for i in $(seq #{niter}); do #{command}; done"
+def run(command, num_iterations)
+  iter_cmd = "for i in $(seq #{num_iterations}); do #{command}; done"
   result = `#{iter_cmd}`
     .scan(/Nodes\/Sec\s*= ([0-9\.]+)/)
     .flatten
     .map(&:to_f)
     .max
   puts result
+end
+
+puts 'serial'
+run 'bin/uts.icc.serial -f inputs/uts/small.input', niter
+puts
+
+puts 'untied'
+num_threads.each do |n|
+  run "OMP_NUM_THREADS=#{n} bin/uts.icc.omp-tasks -f inputs/uts/small.input", niter
+end
+puts
+
+puts 'tied'
+num_threads.each do |n|
+  run "OMP_NUM_THREADS=#{n} bin/uts.icc.omp-tasks-tied -f inputs/uts/small.input", niter
 end
