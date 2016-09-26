eb4544f62643d3a8a60cd38908342e8d6f7f5592
Daisuke Sugiyama <dsugiyama@outlook.jp>
2016-09-27 05:28:35 +0900

    make num_threads variable

--- sparselu.rb
+++ sparselu.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 niter = ARGV[0]
-num_threads = [2, 4, 8, 16, 24, 48]
+num_threads = ARGV[1].split(',').map(&:to_i)
 
 puts 'unit: sec'
 puts "# of threads: #{num_threads.join(' ')}"
