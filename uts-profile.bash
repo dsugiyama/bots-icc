dc71eda4992cdf4f39ae725f89ee515e2fb4111a
Daisuke Sugiyama <dsugiyama@outlook.jp>
2016-10-18 03:25:38 +0900

    add a script for profiling uts

---
+++ uts-profile.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+stack_size_kb=$((16 * 1024))
+ulimit -s $stack_size_kb
+export OMP_STACKSIZE=$stack_size_kb
+export OMP_NUM_THREADS=$2
+export OMP_THREAD_LIMIT=$2
+
+if [[ $3 == tied ]]; then
+    exe=uts.icc.omp-tasks-tied
+else
+    exe=uts.icc.omp-tasks
+fi
+
+amplxe-cl -collect hotspots -result-dir $1 -- \
+    bin/$exe -f inputs/uts/small.input
