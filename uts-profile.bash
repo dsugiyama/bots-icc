1afcf6f153e2e5fa301febbad4f39ccc31b2ab39
Daisuke Sugiyama <dsugiyama@outlook.jp>
2016-10-18 20:56:27 +0900

    further profiling

--- uts-profile.bash
+++ uts-profile.bash
@@ -2,13 +2,19 @@
 stack_size_kb=$((16 * 1024))
 ulimit -s $stack_size_kb
 export OMP_STACKSIZE=$stack_size_kb
-export OMP_NUM_THREADS=$2
 
-if [[ $3 == tied ]]; then
-    exe=uts.icc.omp-tasks-tied
+if [[ $1 = serial ]]; then
+    exe=uts.icc.serial
+    result_dir=$exe
 else
-    exe=uts.icc.omp-tasks
+    export OMP_NUM_THREADS=$1
+    if [[ $2 = tied ]]; then
+        exe=uts.icc.omp-tasks-tied
+    else
+        exe=uts.icc.omp-tasks
+    fi
+    result_dir=${exe}_$1
 fi
 
-amplxe-cl -collect hotspots -result-dir $1 -- \
+amplxe-cl -collect hotspots -result-dir $result_dir -- \
     bin/$exe -f inputs/uts/small.input
