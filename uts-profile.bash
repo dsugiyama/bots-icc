01a7ff6b342d400eff78e5fb5d561ad205ec737c
Daisuke Sugiyama <dsugiyama@outlook.jp>
2016-11-02 09:31:17 +0900

    change OpenMP envvar settings to Argobots' ones

--- uts-profile.bash
+++ uts-profile.bash
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 stack_size_kb=$((16 * 1024))
 ulimit -s $stack_size_kb
-export OMP_STACKSIZE=$stack_size_kb
-export OMP_WAIT_POLICY=ACTIVE
+export ABT_THREAD_STACKSIZE=$stack_size_kb
 
 if [[ $2 = serial ]]; then
     exe=uts.icc.serial
     result_dir=${exe}_$1
 else
-    export OMP_NUM_THREADS=$2
+    export ABT_NUM_ES=$2
     if [[ $3 = tied ]]; then
         exe=uts.icc.omp-tasks-tied
     else
