3367fee25a3d0357a47dc2572e5e0c91ec0e793e
Daisuke Sugiyama <dsugiyama@outlook.jp>
2016-11-19 07:06:11 +0900

    modify execution script

--- uts.rb
+++ uts.rb
@@ -23,9 +23,8 @@ puts
 def run(command, num_iterations)
   iter_cmd = "for i in $(seq #{num_iterations}); do #{command}; done"
   stack_size_kb = 32 * 1024
-  settings  = "ulimit -s #{stack_size_kb}; export OMP_STACKSIZE=#{stack_size_kb}; "
-  settings += "export OMP_WAIT_POLICY=ACTIVE; "
-  settings += "export KMP_TASK_STEALING_CONSTRAINT=0;"
+  settings  = "ulimit -s #{stack_size_kb}; "
+  settings += "export LD_LIBRARY_PATH=$HOME/inst/intel-cilk-runtime/lib:$LD_LIBRARY_PATH;"
   result = `#{settings} #{iter_cmd}`
     .scan(/Nodes\/Sec\s*= ([0-9\.]+)/)
     .flatten
@@ -43,7 +42,7 @@ end
 if task_type == 'untied' || task_type == 'both'
   puts 'untied'
   num_threads.each do |n|
-    run "OMP_NUM_THREADS=#{n} bin/uts.icc.omp-tasks -f inputs/uts/#{workload}.input", niter
+    run "CILK_NWORKERS=#{n} bin/uts.icc.omp-tasks -f inputs/uts/#{workload}.input", niter
   end
   puts
 end
@@ -51,7 +50,7 @@ end
 if task_type == 'tied' || task_type == 'both'
   puts 'tied'
   num_threads.each do |n|
-    run "OMP_NUM_THREADS=#{n} bin/uts.icc.omp-tasks-tied -f inputs/uts/#{workload}.input", niter
+    run "CILK_NWORKERS=#{n} bin/uts.icc.omp-tasks-tied -f inputs/uts/#{workload}.input", niter
   end
   puts
 end
