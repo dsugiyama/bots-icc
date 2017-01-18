dedda0921d19873ab38741d8238ed876025d89c4
Daisuke Sugiyama <dsugiyama@outlook.jp>
2017-01-18 20:58:23 +0900

    実行スクリプトをアップデート

--- uts.rb
+++ uts.rb
@@ -7,13 +7,16 @@ task_type = 'both'
 num_threads = []
 skip_serial = false
 niter = 1
+membind = "";
 
 parser = OptionParser.new
-parser.on('-w', '--workload=VAL') { |v| workload = v }
-parser.on('-t', '--task-type=VAL') { |v| task_type = v }
-parser.on('-n', '--num-threads=VAL') { |v| num_threads = v.split(',').map(&:to_i) }
-parser.on('--skip-serial') { skip_serial = true }
-parser.on('-i', '--num-iterations=VAL') { |v| niter = v }
+parser.on('-w', '--workload=VAL', 'Name of the workload') { |v| workload = v }
+parser.on('-t', '--task-type=VAL', 'Type of OpenMP tasks ("tied", "untied", or "both")') { |v| task_type = v }
+parser.on('-n', '--num-threads=VAL', 'Number of threads') { |v| num_threads = v.split(',').map(&:to_i) }
+parser.on('--skip-serial', 'Skip the execution of serial version') { skip_serial = true }
+parser.on('-i', '--num-iterations=VAL', 'Comma-separated list of number of iterations') { |v| niter = v }
+parser.on('--knl', 'Whether executed on KNL') { membind = "numactl --membind 1" }
+parser.on_tail('-h', '--help', 'Show this message') { puts parser; exit }
 parser.parse!(ARGV)
 
 puts 'unit: nodes/sec'
@@ -36,14 +39,14 @@ end
 
 unless skip_serial
   puts 'serial'
-  run "bin/uts.icc.serial -f inputs/uts/#{workload}.input", niter
+  run "#{membind} bin/uts.icc.serial -f inputs/uts/#{workload}.input", niter
   puts
 end
 
 if task_type == 'untied' || task_type == 'both'
   puts 'untied'
   num_threads.each do |n|
-    run "OMP_NUM_THREADS=#{n} bin/uts.icc.omp-tasks -f inputs/uts/#{workload}.input", niter
+    run "OMP_NUM_THREADS=#{n} #{membind} bin/uts.icc.omp-tasks -f inputs/uts/#{workload}.input", niter
   end
   puts
 end
@@ -51,7 +54,7 @@ end
 if task_type == 'tied' || task_type == 'both'
   puts 'tied'
   num_threads.each do |n|
-    run "OMP_NUM_THREADS=#{n} bin/uts.icc.omp-tasks-tied -f inputs/uts/#{workload}.input", niter
+    run "OMP_NUM_THREADS=#{n} #{membind} bin/uts.icc.omp-tasks-tied -f inputs/uts/#{workload}.input", niter
   end
   puts
 end
