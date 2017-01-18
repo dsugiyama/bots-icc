cbbae14a3b44e3adebce5dd85a941863ae5783da
Daisuke Sugiyama <dsugiyama@outlook.jp>
2017-01-18 22:12:37 +0900

    ラベルの変更に対応

--- uts.rb
+++ uts.rb
@@ -8,6 +8,7 @@ num_threads = []
 skip_serial = false
 niter = 1
 membind = "";
+label = "icc"
 
 parser = OptionParser.new
 parser.on('-w', '--workload=VAL', 'Name of the workload') { |v| workload = v }
@@ -16,6 +17,7 @@ parser.on('-n', '--num-threads=VAL', 'Number of threads') { |v| num_threads = v.
 parser.on('--skip-serial', 'Skip the execution of serial version') { skip_serial = true }
 parser.on('-i', '--num-iterations=VAL', 'Comma-separated list of number of iterations') { |v| niter = v }
 parser.on('--knl', 'Whether executed on KNL') { membind = "numactl --membind 1" }
+parser.on('-l', '--label=VAL', 'Label of executables') { |v| label = v }
 parser.on_tail('-h', '--help', 'Show this message') { puts parser; exit }
 parser.parse!(ARGV)
 
@@ -39,14 +41,14 @@ end
 
 unless skip_serial
   puts 'serial'
-  run "#{membind} bin/uts.icc.serial -f inputs/uts/#{workload}.input", niter
+  run "#{membind} bin/uts.#{label}.serial -f inputs/uts/#{workload}.input", niter
   puts
 end
 
 if task_type == 'untied' || task_type == 'both'
   puts 'untied'
   num_threads.each do |n|
-    run "OMP_NUM_THREADS=#{n} #{membind} bin/uts.icc.omp-tasks -f inputs/uts/#{workload}.input", niter
+    run "OMP_NUM_THREADS=#{n} #{membind} bin/uts.#{label}.omp-tasks -f inputs/uts/#{workload}.input", niter
   end
   puts
 end
@@ -54,7 +56,7 @@ end
 if task_type == 'tied' || task_type == 'both'
   puts 'tied'
   num_threads.each do |n|
-    run "OMP_NUM_THREADS=#{n} #{membind} bin/uts.icc.omp-tasks-tied -f inputs/uts/#{workload}.input", niter
+    run "OMP_NUM_THREADS=#{n} #{membind} bin/uts.#{label}.omp-tasks-tied -f inputs/uts/#{workload}.input", niter
   end
   puts
 end
